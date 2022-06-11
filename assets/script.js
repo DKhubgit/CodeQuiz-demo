7035f10d503d800e0780e81fa07fbc2913d6a3db
DanielK <Danielkang13@gmail.com>
2022-06-11 14:49:35 -0700

    added some quick notes on the initial page and added the button event listener

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var buttonEl = document.getElementById('start');
+
+console.log(buttonEl);
+
+var listQA = [];
+
+function startQuiz(event) {
+    event.preventDefault();
+    alert("hello!");
+}
+
+
+
+
+
+
+buttonEl.addEventListener('click', startQuiz);
