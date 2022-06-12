e3383139bb6ad45a624e92aea91c6426cef079b0
DanielK <Danielkang13@gmail.com>
2022-06-11 18:15:42 -0700

    added the time functionality, tweaked the boxes so that there is no spaces between

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var buttonEl = document.getElementById('start');
 var elementsInsideBox = document.querySelector('.box-elements')
 var boxElements = document.querySelector('.box-start');
 
-var q1 = {question: "Which of following is not a CSS property?",
+var q1 = {question: "1. Which of following is not a CSS property?",
     a1: "background-color",
     a2: "color",
     a3: "font-weight",
@@ -15,6 +15,8 @@ var answerList;
 var questionBox;
 var timerBox;
 var timeCount = 60;
+
+//initializes the layout of the quiz
 function initQuiz(event) {
     event.preventDefault();
     //remove all the inital elements inside the main box
@@ -22,7 +24,7 @@ function initQuiz(event) {
     //update the main box to have a row of child boxes and adjust height
     boxElements.style.height = '500px';
     boxElements.style.flexDirection = 'row';
-    // boxElements.style.justifyContent = 'space-between';
+    boxElements.style.justifyContent = 'space-between';
     //create a box on the left of the main box
     answerBox = document.createElement('div');
     answerBox.className = 'left-box'
@@ -43,24 +45,31 @@ function initQuiz(event) {
     timerBox.className = 'right-box'
     boxElements.append(timerBox);
 
+    //shows the time left for the quiz
     var timeLeft = document.createElement('h2')
     timeLeft.textContent = 'Time Left: ' + timeCount + "sec";
     timerBox.append(timeLeft)
     //add the time interval function to the right box
     
-    // var setTime = setInterval (function () {
-    //     --timerCount;
+    var setTime = setInterval (function () {
+        --timeCount;
+        timeLeft.textContent = 'Time Left: ' + timeCount + "sec";
+
+        if (timeCount === 0) {
+            clearInterval(setTime);
+
+            //make sure we go to the score page after it timer hits 0
+
+        }
+
+    }, 1000)
 
 
-
-    // })
-
-
-    displayQuestions();
+    playQuestions();
 }
 
-
-function displayQuestions() {
+// displays each questions and choices, should return something
+function playQuestions() {
     var dataObject = listQA[0]; //this is an object
 
     var elements = document.createElement('h2');
@@ -68,26 +77,21 @@ function displayQuestions() {
     questionBox.append(elements);
 
     var elements1 = document.createElement('button')
-    elements1.textContent = "1. " + dataObject.a1;
+    elements1.textContent = "A. " + dataObject.a1;
     questionBox.append(elements1);
 
     var elements2 = document.createElement('button')
-    elements2.textContent = "2. " + dataObject.a2;
+    elements2.textContent = "B. " + dataObject.a2;
     questionBox.append(elements2);
 
     var elements3 = document.createElement('button')
-    elements3.textContent = "3. " + dataObject.a3;
+    elements3.textContent = "C. " + dataObject.a3;
     questionBox.append(elements3);
 
     var elements4 = document.createElement('button')
-    elements4.textContent = "4. " + dataObject.a4;
+    elements4.textContent = "D. " + dataObject.a4;
     questionBox.append(elements4);
-    // for (var i = 1; i < 5; i++) {
-    //     elements2.textcontent = dataObject[1]
-    // }
-    // elements.textContent = dataObject.question;
-    // boxElements.append(elements);
-    // console.log(boxElements);
+
     return;
 }
 
